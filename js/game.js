79159fb7f0e18b1981499c482656eef96de5c0ca
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-08-18 14:54:07 -0300

    feat: game mosca

--- js/game.js
+++ js/game.js
@@ -8,12 +8,18 @@ function ajustarTelaGame(){
 ajustarTelaGame()
 
 function posicaoRandomica(){
-  let posicaoX = Math.floor(Math.random() * largura)
-  let posicaoY = Math.floor(Math.random() * altura)
+  let posicaoX = Math.floor(Math.random() * largura) - 90
+  let posicaoY = Math.floor(Math.random() * altura) - 90
+
+  posicaoX = posicaoX < 0 ? 0 : posicaoX
+  posicaoY = posicaoY < 0 ? 0 : posicaoY
 
   let mosca = document.createElement('img')
   mosca.src = "/img/mosca.png"
   mosca.className = 'mosca1'
+  mosca.style.left = posicaoX + 'px'
+  mosca.style.top = posicaoY + 'px'
+  mosca.style.position = 'absolute'
 
   document.body.appendChild(mosca)
 }
