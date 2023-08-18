3f7d69b9b1fe0f69df9b35bbc975c50e98a730ad
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-08-18 16:07:45 -0300

    feat: game mosca

--- js/game.js
+++ js/game.js
@@ -8,6 +8,11 @@ function ajustarTelaGame(){
 ajustarTelaGame()
 
 function posicaoRandomica(){
+
+  if(document.getElementById('mosca')){
+    document.getElementById('mosca').remove()
+  }
+
   let posicaoX = Math.floor(Math.random() * largura) - 90
   let posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -20,7 +25,7 @@ function posicaoRandomica(){
   mosca.style.left = posicaoX + 'px'
   mosca.style.top = posicaoY + 'px'
   mosca.style.position = 'absolute'
-
+  mosca.id = 'mosca'
   document.body.appendChild(mosca)
 
 }
